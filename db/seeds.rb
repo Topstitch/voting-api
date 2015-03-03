09eb92d8a46053252b50d5818900de32496e1783
Topstitch <kallen@spoonflower.com>
2015-03-03 18:07:11 -0500

    did seeds, seems to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Candidate.create(name:'Sally Smartypants', party:'Goat')
+Candidate.create(name:'Olivia Openminded', party:'Goat')
+Candidate.create(name:'Johnny Justice', party:'Sheep')
+Candidate.create(name:'Colleen Caring', party:'Sheep')
+
+Voter.create(name:'Alice', party:'Goat')
+Voter.create(name:'Brian', party:'Goat')
+Voter.create(name:'Caroline', party:'Goat')
+Voter.create(name:'Donald', party:'Sheep')
+Voter.create(name:'Elizabeth', party:'Sheep')
+Voter.create(name:'Frank', party:'Sheep')
+
+Vote.create(voter_id: 1, candidate_id: 1)
+Vote.create(voter_id: 2, candidate_id: 1)
+Vote.create(voter_id: 3, candidate_id: 1)
+Vote.create(voter_id: 4, candidate_id: 2)
+Vote.create(voter_id: 5, candidate_id: 3)
+Vote.create(voter_id: 6, candidate_id: 4)
